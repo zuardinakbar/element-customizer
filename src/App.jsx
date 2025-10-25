550d275b7f7ab107a60d902b764e25f32113893b
ZuardinAkbar <zuardin.akbar@icd.uni-stuttgart.de>
2025-10-26 00:32:20 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './style.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='app-container'>
+      <h1>Element Customizer</h1>
+    </div>
+  )
+}
+
+export default App
